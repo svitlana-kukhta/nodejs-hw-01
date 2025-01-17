a8dbb97aed7a9f2d197e26748310a1a4cb5edd92
svitlana-kukhta <tyschenkosvetlana@gmail.com>
2025-01-17 15:21:20 +0200

    goit-node-hw-01-2

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,8 @@
-export const PATH_DB = './src/db/db.json';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+export const PATH_DB = path.resolve(__dirname, '../db/db.json');
